edb5feeb2272f3d6fd1fb20725b302cbc2fc52bd
Juan Chuc <dev.juan.eloper@gmail.com>
2024-11-13 23:59:10 -0600

    feature: styles for component loader

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,14 +1,13 @@
 import { Loader } from "~components/Loader";
-import { currentUserSelector } from "~redux/selectors/auth.selector";
-import { useAppSelector } from "~redux/store";
 
 export const Auth = () => {
-    const authState = useAppSelector(currentUserSelector);
-    console.log(authState);
     return (
         <div>
             <h1>Auth</h1>
             <Loader />
+            <a href="https://uiverse.io/inputs?page=2" target="_blank">
+                Sign In
+            </a>
         </div>
     );
 };
