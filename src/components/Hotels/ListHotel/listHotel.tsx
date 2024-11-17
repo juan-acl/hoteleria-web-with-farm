86c62d8243f7510da3c4008d5c2b02624dc79030
Juan Chuc <dev.juan.eloper@gmail.com>
2024-11-16 21:29:42 -0600

    styles: grid layout for list item hotel

--- src/components/Hotels/ListHotel/listHotel.tsx
+++ src/components/Hotels/ListHotel/listHotel.tsx
@@ -1,9 +1,19 @@
 import { ItemHotel } from "../ItemHotel/itemHotel";
+import "./listHotel.css";
 
 export const ListHotel = () => {
     return (
-        <>
+        <div className="container__list__hotel">
             <ItemHotel />
-        </>
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+            <ItemHotel />
+        </div>
     );
 };
