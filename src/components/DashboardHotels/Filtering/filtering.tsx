d28e190bd45496d914af668fa5f578e7a9bbb56b
Juan Chuc <dev.juan.eloper@gmail.com>
2024-11-22 23:52:45 -0600

    feature: filtering count

--- src/components/DashboardHotels/Filtering/filtering.tsx
+++ src/components/DashboardHotels/Filtering/filtering.tsx
@@ -11,21 +11,71 @@ export const Filtering = () => {
                     <h5 className="text__price__range">Price Range</h5>
                 </div>
                 <div className="items__star__raiting">
-                    <div className="item__star__raiting">
-                        <input type="checkbox" name="star" id="star1" />
-                        <label htmlFor="star1">1 Star</label>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            padding: "5px",
+                        }}
+                    >
+                        <div>
+                            <input type="checkbox" name="star" id="star1" />
+                            <label htmlFor="star1">$ 0 - $ 200 </label>
+                        </div>
+                        <div>
+                            <label>11</label>
+                        </div>
                     </div>
                     <div className="item__star__raiting">
-                        <input type="checkbox" name="star" id="star2" />
-                        <label htmlFor="star2">2 Star</label>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                padding: "5px",
+                            }}
+                        >
+                            <div>
+                                <input type="checkbox" name="star" id="star2" />
+                                <label htmlFor="star2">$ 200 - $ 500</label>
+                            </div>
+                            <div>
+                                <label>100</label>
+                            </div>
+                        </div>
                     </div>
                     <div className="item__star__raiting">
-                        <input type="checkbox" name="star" id="star3" />
-                        <label htmlFor="star3">3 Star</label>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                padding: "5px",
+                            }}
+                        >
+                            <div>
+                                <input type="checkbox" name="star" id="star3" />
+                                <label htmlFor="star3">3 Star</label>
+                            </div>
+                            <div>
+                                <label>22</label>
+                            </div>
+                        </div>
                     </div>
                     <div className="item__star__raiting">
-                        <input type="checkbox" name="star" id="star4" />
-                        <label htmlFor="star4">4 Star</label>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                padding: "5px",
+                            }}
+                        >
+                            <div>
+                                <input type="checkbox" name="star" id="star4" />
+                                <label htmlFor="star4">4 Star</label>
+                            </div>
+                            <div>
+                                <label>45</label>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
